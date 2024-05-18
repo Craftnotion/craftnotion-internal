ac3eb1027327b795fc7c7f10706966a26ea2c2f2
Dinesh <dineshchaudhary923092@gmail.com>
2024-05-18 11:09:51 +0530

    refactor: internal login screen

---
+++ src/app/app/internal/management/projects/page.tsx
@@ -0,0 +1,14 @@
+/** React Imports */
+import React from 'react'
+
+type Props = {}
+
+const ManagementProjectsPage = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ManagementProjectsPage
